b70f1c237db44d2316f1697b9d352cdff0856682
Ida Wang <iwang2@stuy.edu>
2017-02-06 17:48:28 -0500

    mostly working, but some weird answers for smaller numbers (1, 2)

---
+++ 01Recursion/Recursion.java
@@ -0,0 +1,36 @@
+public class Recursion{
+    public static String name(){
+	return "Wang,Ida";
+    }
+
+    public static double sqrt(double n){
+	if(n < 0){
+	    throw new IllegalArgumentException();
+	}else{
+	    return helper(n, n / 2);
+	}
+    }
+
+    public static double helper(double n, double guess){
+	if(n == 0){
+	    return 0;
+	}else if(guess * guess / n < 1.00000000001){
+	    return guess;
+	}else{
+	    return helper(n, (n / guess + guess) / 2);
+	}
+    }
+
+    public static void main(String[]args){
+	try{
+	    System.out.print(args[0] + ": ");
+	    System.out.println(sqrt(Double.parseDouble(args[0])));
+	}
+	catch(IllegalArgumentException e){
+	    System.out.println("input must be greater than or equal to 0");
+	}
+	catch(IndexOutOfBoundsException o){
+	    System.out.println("Please type in a number to calculate its square root.");
+	}
+    }
+}
