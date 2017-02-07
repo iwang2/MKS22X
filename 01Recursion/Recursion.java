62a63d0176503d5c2b9687abe08e488303cab1fe
Ida Wang <iwang2@stuy.edu>
2017-02-07 15:21:38 -0500

    fixed some minor stuff / testing from 1 --> 100 and testing perfect squares

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -6,8 +6,10 @@ public class Recursion{
     public static double sqrt(double n){
 	if(n < 0){
 	    throw new IllegalArgumentException();
+	}else if(n < 1){
+	    return helper(n, 1);
 	}else if(n < 4){
-	    return helper(n, 4);
+	    return helper(n, n);
 	}else{
 	    return helper(n, n / 2);
 	}
@@ -16,8 +18,7 @@ public class Recursion{
     public static double helper(double n, double guess){
 	if(n == 0){
 	    return 0;
-	}else if(guess * guess / n < 1.000000001){
-	    System.out.print(guess * guess + ": ");
+	}else if(guess * guess / n < 1.000000000000001){
 	    return guess;
 	}else{
 	    return helper(n, (n / guess + guess) / 2);
@@ -25,6 +26,7 @@ public class Recursion{
     }
 
     public static void main(String[]args){
+	//System.out.println(name());
 	try{
 	    System.out.print(args[0] + ": ");
 	    System.out.println(sqrt(Double.parseDouble(args[0])));
@@ -32,8 +34,14 @@ public class Recursion{
 	catch(IllegalArgumentException e){
 	    System.out.println("input must be greater than or equal to 0");
 	}
-	catch(IndexOutOfBoundsException o){
-	    System.out.println("Please type in a number to calculate its square root.");
+	catch(IndexOutOfBoundsException b){
+	    for(int i = 0; i <= 100; i++){
+		System.out.println(i + ": " + sqrt(i));
+	    }
+	    for (int i = 0; i < 16; i++){
+		System.out.println(i*i + ": " + sqrt(i*i));
+	    }
+	    //System.out.println("Please type in a number to calculate its square root.");
 	}
     }
 }
