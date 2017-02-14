f7c2e40d36732306a261cfaa74be7c555eab25f5
Ida Wang <iwang2@stuy.edu>
2017-02-13 23:10:56 -0500

    fully functional - counting solutions is somewhat reasonable up to 13/14

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -55,32 +55,43 @@ public class QueenBoard{
     
     //find 1st solution and stops; updates toString board
     public boolean solve(){
-	return solveH(0,0);
+	return solveH(0);
     }
-    private boolean solveH(int row, int col){
+    private boolean solveH(int col){
 	if(col >= board.length){
 	    return true;
 	}
-	if(row >= board.length && col < 0){
-	    return false;
-	}else if(row >= board.length){
-	    for(int i = 0; i < board.length; i++){
-		if(board[i][col-1] < 0){
-		    removeQueen(i,col-1);
-		    return solveH(i+1,col);
+        for(int r = 0; r < board.length; r++){
+	    if(board[r][col] == 0){
+		addQueen(r,col);
+	        if(solveH(col+1)){
+		    return true;
+		}else{
+		    removeQueen(r,col);
 		}
 	    }
-	}else if(board[row][col] == 0){
-	    addQueen(row,col);
-	    return solveH(0,col+1);
-	}else{
-	    return solveH(row+1,col);
 	}
 	return false;
     }
-    
+        
     //look for all solutions, updates solutionCount
     public void countSolutions(){
+        counter(0);
+	solutionCount++;
+    }
+    private boolean counter(int col){
+	if(col >= board.length){
+	    solutionCount++;
+	    return true;
+	}
+	for(int r = 0; r < board.length; r++){
+	    if(board[r][col] == 0){
+		addQueen(r,col);
+	        counter(col+1);
+		removeQueen(r,col);
+	    }
+	}
+	return false;
     }
 
     //returns number of solutions, or -1 if solver was never run
@@ -106,18 +117,20 @@ public class QueenBoard{
 
     public static void main(String[]args){
 	try{
-	    QueenBoard input = new QueenBoard(Integer.parseInt(args[0]));
-	    System.out.println(input.solve());
-	    System.out.println(input);
+	    int input = Integer.parseInt(args[0]);
+	    QueenBoard a = new QueenBoard(input);
+	    //System.out.println(a.solve());
+	    //System.out.println(a);
+	    a.countSolutions();
+	    System.out.println(a.getCount());
 	}
-	catch(IndexOutOfBoundsException e){	    
-	    QueenBoard a = new QueenBoard(5);
-	    System.out.println(a.solve());
-	    System.out.println(a);
-
-	    QueenBoard b = new QueenBoard(10);
-	    System.out.println(b.solve());
-	    System.out.println(b);
+	catch(IndexOutOfBoundsException e){
+	    QueenBoard a;
+	    for(int i = 0; i < 11; i++){
+		a = new QueenBoard(i);
+		a.countSolutions();
+		System.out.println(i + ": " + a.getCount());
+	    }
 	}
     }
 }
