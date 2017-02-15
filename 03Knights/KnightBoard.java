d0599a6b3538d5b21809e3e6e67069a718f122a9
Ida Wang <iwang2@stuy.edu>
2017-02-15 17:54:00 -0500

    fully functional

--- 03Knights/KnightBoard.java
+++ 03Knights/KnightBoard.java
@@ -2,12 +2,17 @@ public class KnightBoard{
     private int[][] board;
     private boolean solved;
     private int size;
+    private int wide;
+    private int tall;
     
     private int[][] direction = new int[8][2];
     
-    public KnightBoard(int row, int col){
-	board = new int[row][col];
-	size = row * col;
+    public KnightBoard(int width, int length){
+	wide = width;
+	tall = length;
+	size = wide * tall;
+	
+	board = new int[wide][tall];
 	solved = false;
 	
 	direction[0][0] = 1; direction[0][1] = 2;
@@ -21,26 +26,32 @@ public class KnightBoard{
     }
 
     public void solve(){
-	for(int r = 0; r < board.length; r++){
-	    for( int c = 0; c < board[0].length; c++){
+	int row = wide / 2 + wide % 2;
+	int col = tall / 2 + tall % 2;
+	for(int r = 0; r < row; r++){
+	    for( int c = r; c < col; c++){
 		if(solveH(r,c,1)){
+		    solved = true;
 		    return;
 		}
 	    }
 	}
     }
+    
     private boolean solveH(int row, int col, int level){
-	if(level >= size){
-	    solved = true;
+	if(level > size){
 	    return true;
-	}else if(board[row][col] == 0){
-	    board[row][col] = level + 1;
+	}
+	else if(board[row][col] == 0){
+	    board[row][col] = level;
+
 	    for(int i = 0; i < 8; i++){
 		int r = row + direction[i][0];
 		int c = col + direction[i][1];
-		if(r >= 0 && r < board.length &&
-		   c >= 0 && c < board[0].length &&
-		   solveH(r, c, level + 1)){
+
+		if((wide == 1 && tall == 1) ||
+		   (r >= 0 && r < wide && c >= 0 && c < tall
+		    && solveH(r,c,level+1))){
 		    return true;
 		}
 	    }
@@ -53,8 +64,8 @@ public class KnightBoard{
     public String toString(){
 	String ans = "";
 	if(solved){
-	    for(int r = 0; r < board.length; r++){
-		for(int c = 0; c < board[0].length; c++){
+	    for(int r = 0; r < wide; r++){
+		for(int c = 0; c < tall; c++){
 		    if(board[r][c] < 10){
 			ans += " " + board[r][c] + " ";
 		    }else{
@@ -68,8 +79,11 @@ public class KnightBoard{
     }
 
     public static void main(String[]args){
-	KnightBoard a = new KnightBoard(4,4);
-	a.solve();
-	System.out.println(a);
+	if(args.length == 2){
+	    KnightBoard a = new KnightBoard(Integer.parseInt(args[0]),
+					    Integer.parseInt(args[1]));
+	    a.solve();
+	    System.out.println(a);
+	}
     }
 }
