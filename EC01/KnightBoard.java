47df7e41c6966fc3e1f13e7e6674d704b4e16492
Ida Wang <iwang2@stuy.edu>
2017-02-17 23:06:30 -0500

    probably the worst way to do this, but I moved the version from class to the EC directory. there was a lot of copy pasting involved

--- EC01/KnightBoard.java
+++ EC01/KnightBoard.java
@@ -1,5 +1,6 @@
 public class KnightBoard{
     private int[][] board;
+    private int[][] less;
     private boolean solved;
     private int size;
     private int wide;
@@ -13,6 +14,7 @@ public class KnightBoard{
 	size = wide * tall;
 	
 	board = new int[wide][tall];
+	less = new int[wide][tall];
 	solved = false;
 	
 	direction[0][0] = 1; direction[0][1] = 2;
@@ -23,6 +25,21 @@ public class KnightBoard{
 	direction[5][0] = 2; direction[5][1] = -1;
 	direction[6][0] = -2; direction[6][1] = 1;
 	direction[7][0] = -2; direction[7][1] = -1;
+
+	for(int r = 0; r < wide; r++){
+	    for(int c = 0; c < tall; c++){
+		int pos = 0; 
+		for(int i = 0; i < 8; i++){
+		    int newr = r + direction[i][0];
+		    int newc = c + direction[i][1];
+		    if(newr >= 0 && newr < wide &&
+		       newc >= 0 && newc < tall){
+			pos++;
+		    }
+		}
+		less[r][c] = pos;
+	    }
+	}
     }
 
     public void solve(){
@@ -60,6 +77,38 @@ public class KnightBoard{
 	return false;
     }
 
+    public void solveFast(){
+	int row = wide / 2 + wide % 2;
+	int col = tall / 2 + tall % 2;
+	for(int r = 0; r < row; r++){
+	    for(int c = r; c < col; c++){
+		if(faster(r,c,1)){
+		    solved = true;
+		    return;
+		}
+	    }
+	}
+    }
+    public boolean faster(int row, int col, int level){
+	if(level > size){
+	    return true;
+	}
+	else if(board[row][col] == 0){
+	}
+	return false;
+    }
+    public int[][] arr(int row, int col){
+	int options = 0;
+	for(int i = 0; i < 8; i++){
+	    int r = row + direction[i][0];
+	    int c = row + direction[i][1];
+
+	    if(r >= 0 && r < wide && c >= 0 && c < tall){
+		options++;
+	    }
+	}
+    }
+    
     //blank if never called or no solution
     public String toString(){
 	String ans = "";
@@ -77,11 +126,23 @@ public class KnightBoard{
 	}
 	return ans;
     }
+    public String toString2(){
+	String ans = "";
+	for(int r = 0; r < wide; r++){
+	    for(int c = 0; c < tall; c++){
+		ans += less[r][c] + " ";
+	    }
+	    ans += "\n";
+	}
+	return ans;
+    }
 
     public static void main(String[]args){
 	if(args.length == 2){
 	    KnightBoard a = new KnightBoard(Integer.parseInt(args[0]),
 					    Integer.parseInt(args[1]));
+	    
+	    System.out.println(a.toString2());
 	    a.solve();
 	    System.out.println(a);
 	}
