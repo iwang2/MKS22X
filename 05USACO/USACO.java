ac170661d0a24540879afd457a28937de7ede3f0
Ida Wang <iwang2@stuy.edu>
2017-03-12 22:01:35 -0400

    untested, but printing of arrays look correct

--- 05USACO/USACO.java
+++ 05USACO/USACO.java
@@ -70,13 +70,82 @@ public class USACO{
 	}
     }
 
+    public static int silver(String file){
+	Scanner scan = new Scanner("");
+	try{
+	    scan = new Scanner(new File(file));
+	}catch(FileNotFoundException n){
+	    System.out.println("Invalid filename");
+	    System.exit(1);
+	}
+
+	int length = scan.nextInt(), width = scan.nextInt(),
+	    steps = scan.nextInt();
+	
+	int[][] pasture = new int[length][width], count = new int[length][width];
+	for(int r = 0; r < length; r++){
+	    String line = scan.next();
+	    for(int c = 0; c < width; c++){
+		if(line.charAt(c) == '.'){
+		    pasture[r][c] = 0;
+		}else{
+		    pasture[r][c] = -1;
+		}
+		count[r][c] = 0;
+	    }
+	}
+
+	int startr = scan.nextInt() - 1, startc = scan.nextInt() - 1;
+	int endr = scan.nextInt() - 1, endc = scan.nextInt() - 1;
+
+	pasture[startr][startc] = 1;
+
+	for(int i = 0; i <= steps; i++){
+	    for(int r = 0; r < length; r++){
+		for(int c = 0; c < width; c++){
+		    if(count[r][c] < i && pasture[r][c] > 0){
+			if(r+1 < length && pasture[r+1][c] > -1){
+			    pasture[r+1][c] += pasture[r][c];
+			    count[r+1][c] = i;
+			}
+			if(r-1 >= 0 && pasture[r-1][c] > -1){
+			    pasture[r-1][c] += pasture[r][c];
+			    count[r-1][c] = i;
+			}
+			if(c+1 < width && pasture[r][c+1] > -1){
+			    pasture[r][c+1] += pasture[r][c];
+			    count[r][c+1] = i;
+			}
+			if(c-1 >= 0 && pasture[r][c-1] > -1){
+			    pasture[r][c-1] += pasture[r][c];
+			    count[r][c-1] = i;
+			}
+			count[r][c]++;
+			pasture[r][c] = 0;
+		    }else if(count[r][c] < i){
+			count[r][c]++;
+		    }
+		}
+	    }
+	    print(pasture);
+	    System.out.println();
+	}
+	return pasture[endr][endc];
+    }
+
     public static void main(String[]args){
+	/*
 	try{
 	    for(int i = 1; i < 11; i++){
 		System.out.print("test " + i + ": ");		
 		Scanner scan = new Scanner(new File("makelake." + i + ".out"));
 		System.out.println(bronze("makelake." + i + ".in") == scan.nextInt());
 	    }
-	}catch(FileNotFoundException e){}
+	}catch(FileNotFoundException e){
+	    System.out.println("Invalid filename");
+	    System.exit(1);
+	}
+	*/
+	silver("ctravel.1.in");
     }
 }
