f9064c263ff2e64d2de9f565f0563f05cd74c863
Ida <iwang2@stuy.edu>
2017-03-28 15:33:43 -0400

    pretty sure the add method is completely wrong

--- 09LinkedList/MyLinkedList.java
+++ 09LinkedList/MyLinkedList.java
@@ -1,7 +1,51 @@
-public class MyLinkedList{
-    LNode start;
-    int size;
-    
+public class MyLinkedList{    
     private class LNode{
+	private int value;
+	private LNode next;
+
+	public LNode(int value, LNode next){
+	    this.value = value;
+	    this.next = next;
+	}
+    }
+    
+    private LNode start;
+    private int size;
+
+    public MyLinkedList(){
+	size = 0;
+	start = new LNode(null, null);
+    }
+    
+    //EXCEPTIONS!!!
+    public boolean add(int value){
+	LNode temp = start;
+	for(int i = 0; i < size; i++){
+	    temp = temp.next;
+	}
+	temp.value = value;
+	size++;
+	return true;
+    }
+
+    public int size(){
+	return size;
+    }
+
+    public String toString(){
+	LNode node = start;
+	System.out.print("[");
+	for(int i = 0; i < size; i++){
+	    System.out.print(node.value + " ");
+	    node = node.next;
+	}
+    }
+
+    //EXCEPTIONS!!!
+    public int get(int index){
+    }
+
+    //EXCEPTIONS!!!
+    public int set(int index, int newValue){
     }
 }
