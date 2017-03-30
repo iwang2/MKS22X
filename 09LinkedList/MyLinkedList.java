899c25fb251f52bc20134d97f86d92d060e3da71
Ida Wang <iwang2@stuy.edu>
2017-03-30 10:00:23 -0400

    add is weird and doesn't really work, or maybe the toString is broken

--- 09LinkedList/MyLinkedList.java
+++ 09LinkedList/MyLinkedList.java
@@ -1,4 +1,5 @@
-public class MyLinkedList{    
+public class MyLinkedList{
+    
     private class LNode{
 	private int value;
 	private LNode next;
@@ -14,18 +15,26 @@ public class MyLinkedList{
 
     public MyLinkedList(){
 	size = 0;
-	start = new LNode(null, null);
+	start = null;
     }
     
     //EXCEPTIONS!!!
     public boolean add(int value){
-	LNode temp = start;
+	if(size == 0){
+	    start = new LNode(value, null);
+	}else{
+	    LNode temp = start;
+	    start.value = value;
+	    start.next = temp;
+	}
+	/*
 	for(int i = 0; i < size; i++){
 	    temp = temp.next;
 	}
 	temp.value = value;
+	*/
 	size++;
-	return true;
+	return true;	
     }
 
     public int size(){
@@ -34,18 +43,42 @@ public class MyLinkedList{
 
     public String toString(){
 	LNode node = start;
+	String ans = "";
 	System.out.print("[");
 	for(int i = 0; i < size; i++){
-	    System.out.print(node.value + " ");
+	    ans = node.value + " ";
 	    node = node.next;
 	}
+	return ans.substring(0, ans.length() - 1) + "]";
     }
 
     //EXCEPTIONS!!!
     public int get(int index){
+	LNode temp = start;
+	for(int i = 0; i <= index; i++){
+	    temp = temp.next;
+	}
+	return temp.value;
     }
 
     //EXCEPTIONS!!!
     public int set(int index, int newValue){
+	LNode temp = start;
+	int old = get(index);
+	
+	for(int i = 0; i <= index; i++){
+	    temp = temp.next;
+	}
+
+	temp.value = newValue;
+	return old;
+    }
+
+    public static void main(String[] args){
+	MyLinkedList a = new MyLinkedList();
+	a.add(2);
+	a.add(-4);
+	a.add(100);
+	System.out.println(a);
     }
 }
