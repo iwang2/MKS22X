4c2a29890d3d3d4d490ee02a89e5d957bfbea773
Ida <iwang2@stuy.edu>
2017-05-04 15:33:45 -0400

    stuff isnt working

--- 13MyHeap/MyHeap.java
+++ 13MyHeap/MyHeap.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class MyHeap{
     
     private ArrayList<String> heap;
@@ -8,7 +10,7 @@ public class MyHeap{
 	b = true;
 	heap = new ArrayList<String>();
 	heap.add("");
-	tail = 1;
+	tail = 0;
     }
 
     //true - max heap, false - min heap
@@ -16,31 +18,77 @@ public class MyHeap{
 	this.b = b;
 	heap = new ArrayList<String>();
 	heap.add("");
+	tail = 0;
     }
 
     public void add(String s){
 	heap.add(s);
 	tail++;
-	up(tail);
+	if(b){
+	    upM(tail);
+	}else{
+	    //upm(tail);
+	}
     }
 
     public String remove(){
+	String ans = heap.get(1);
+	heap.set(1, heap.get(tail));
 	heap.remove(tail);
 	tail--;
+	if(b){
+	    downM(1);
+	}else{
+	    //downm(1);
+	}
+	return ans;
     }
 
     public String peek(){
 	return heap.get(tail);
     }
-
-    //max --> min
-    private void up(int index){
-	if(index != 1 || heap.get(index / 2).compareTo(heap.get(index)) < 0){
+    
+    private void upM(int index){
+	if(index != 1 && heap.get(index / 2).compareTo(heap.get(index)) < 0){
 	    swap(index, index / 2);
-	    up(index / 2);
+	    upM(index / 2);
 	}
     }
 
-    private void down(){
+    private void downM(int index){
+	if(index > tail / 2){
+	    return;
+	}else if(heap.get(index).compareTo(heap.get(index*2)) < 0){
+	    swap(index, index*2);
+	    downM(index*2);
+	}else if(heap.get(index).compareTo(heap.get(index*2 + 1)) < 0){
+	    swap(index, index*2 + 1);
+	    downM(index*2 + 1);
+	}
+    }
+
+    private void swap(int in1, int in2){
+	String hold = heap.get(in1);
+	heap.set(in1, heap.get(in2));
+	heap.set(in2, hold);
+    }
+
+    public void print(){
+	for(int i = 1; i <= tail; i *= 2){
+	    for(int j = i; j < i*2; j++){
+		System.out.print(heap.get(j) + " ");
+	    }
+	    System.out.println();
+	}
+    }
+
+    public static void main(String[] args){
+	MyHeap a = new MyHeap();
+	a.add("b");
+	a.add("d");
+	a.add("a");
+	a.add("e");
+	a.add("c");
+	a.print();
     }
 }
